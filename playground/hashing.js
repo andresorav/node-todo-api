7801fad42c37ed799c1ac6dad127a3edfda74ebc
Andres Orav <andres.orav@pipedrive.com>
2018-09-01 14:35:22 +0300

    Hash user password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,5 +1,20 @@
 const {SHA256} = require('crypto-js');
 const {sign, verify} = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+const password = 'kalamaja1234';
+const hashedPassword = '$2a$10$M3RwWlLnFPVqzCtgndB/QenX9pY/XwP.OTlf3xH/6SxUQSLEE.k8W';
+
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log('Hash:',  hash);
+  });
+});
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(err, res);
+});
+/*
 
 const message = 'Some random string';
 const hash = SHA256(message).toString();
@@ -16,3 +31,4 @@ console.log('Token: ', token);
 
 const decoded = verify(token, 'salt1234');
 console.log('Decoded', decoded);
+*/
