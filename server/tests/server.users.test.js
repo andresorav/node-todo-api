53abbd133c55add6c52a0bb9359a61c1a6c3c173
Andres Orav <andres.orav@pipedrive.com>
2018-09-15 11:41:12 +0300

    Updated user login tests

--- server/tests/server.users.test.js
+++ server/tests/server.users.test.js
@@ -98,18 +98,53 @@ describe('GET /users/me', () => {
 });
 
 describe('POST /users/login', () => {
-  it('should return user on success', (done) => {
+  it('should login user and return auth token', (done) => {
     request(app)
       .post('/users/login')
       .send({
-        email: users[0].email,
-        password: users[0].password
+        email: users[1].email,
+        password: users[1].password
       })
       .expect(200)
       .expect((res) => {
-        expect(res.body._id).toBe(users[0]._id.toHexString());
-        expect(res.body.email).toBe(users[0].email);
+        expect(res.headers['x-auth']).toBeTruthy();
       })
-      .end(done);
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user).toBeTruthy();
+          expect(user.tokens[0]).toMatchObject({
+            access: 'auth',
+            token: res.headers['x-auth']
+          });
+          done();
+        }).catch((err) => done(err));
+      });
+  });
+
+  it('should reject invalid login', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: 'randomPass1234'
+      })
+      .expect(400)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toBeFalsy();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((err) => done(err));
+      });
   });
 });
